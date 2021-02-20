b8299e71ece8e8567e87cf80e76621907d2bc0e1
ZakiMohammed <shaikhzaki67@gmail.com>
2021-02-20 20:25:20 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# PokedexApp
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.1.
+
+```
+https://pokeapi.co/
+https://pokeres.bastionbot.org/
+```
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { ViewComponent } from './components/view/view.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    ViewComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
